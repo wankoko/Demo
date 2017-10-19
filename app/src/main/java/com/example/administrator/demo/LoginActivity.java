57b6bc9a0d23d0528959d8d18c96de550764a97f
Wan <1285165201@qq.com>
2017-10-19 11:37:54 +0800

    Button

--- app/src/main/java/com/example/administrator/demo/LoginActivity.java
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button b = (Button) findViewById(R.id.Login_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
